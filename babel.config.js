b0a2e4bf6206fee9a2aa18f9e52ad898ecf074be
Adnan Khattak <adnanktk540@gmail.com>
2025-12-18 10:18:27 +0500

    Feat: TS setup added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
